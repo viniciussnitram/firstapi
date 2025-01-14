6b637af627e0cdc85faf80f805502f8fbe3750ef
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-01-13 22:48:59 -0300

    feat: create a mock to simulate a database returning users, create routesand create controllers

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const routes = require('./routes');
+
+const server = http.createServer((request, response) => {
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  const route = routes.find((routeObject) => (
+    routeObject.endpoint === request.url && routeObject.method === request.method
+  ));
+
+  if (route) {
+    route.handler(request, response);
+  } else {
+    response.writeHead(404, { 'Content-type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+});
+
+server.listen(3000, () => console.log('Server started at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Vinicius',
+  },
+  {
+    id: 2,
+    name: 'Arrascaeta',
+  },
+  {
+    id: 3,
+    name: 'Nosferatu',
+  }
+];

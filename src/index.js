01119df2ec734da9a1a27af740854b321f82d6b8
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-01-15 19:31:25 -0300

    feat: add query params

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const http = require('http');
+const { URL } = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === request.url && routeObject.method === request.method
+    routeObject.endpoint === parsedUrl.pathname && routeObject.method === request.method
   ));
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-type': 'text/html' });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 

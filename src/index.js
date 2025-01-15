39650b2a18a80a988a8e3cab44b9f708eb42bacc
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-01-15 19:47:25 -0300

    feat: receiving parameters in the URL

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`);
   console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === parsedUrl.pathname && routeObject.method === request.method
+    routeObject.endpoint === pathname && routeObject.method === request.method
   ));
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
 
     route.handler(request, response);
   } else {
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,10 @@ module.exports = [
     endpoint: '/users',
     method: 'GET',
     handler: UserController.listUsers,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: UserController.getUserById,
   }
 ];
